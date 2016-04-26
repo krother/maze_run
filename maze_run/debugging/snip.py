413bcfabac79ca5bb8df17ccbadf9bffc23354ed
krother <krother@academis.eu>
2016-04-27 00:57:47 +0200

    cleanup work

--- maze_run/debugging/snip.py
+++
@@ -1,4 +0,0 @@
-
-def get_tile_rect(x, y):
-    """Converts tile indices to a pygame.Rect"""
-    return 
--- maze_run/debugging/test.py
+++
@@ -1,8 +0,0 @@
-
-
-#numbers = [1, 4, 9, 16, 25]
-
-for n in range(100, 0, -5):
-    print(n)
-
-"bla".split()

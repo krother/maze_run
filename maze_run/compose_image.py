ed8574b8f1b123f96917cf21afb84cc93d27300e
krother <krother@academis.eu>
2016-05-21 06:59:44 +0200

    moved part2

--- maze_run/compose_image.py
+++
@@ -1,9 +0,0 @@
-
-from pygame import image, Rect
-
-bg = image.load('bg.xpm')
-ex = image.load('explo.xpm')
-
-bg.blit(ex, Rect((15, 15, 0, 0)), Rect((0, 0, 32, 32)))
-
-image.save(bg, 'merged.png')

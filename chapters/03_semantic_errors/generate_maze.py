d92e6fc1f70d1843beb782c066e15546dbd83f2c
krother <krother@academis.eu>
2017-04-03 20:30:05 +0200

    moved chapter 03/04 to final location

--- chapters/03_semantic_errors/generate_maze.py
+++ chapters/03_semantic_errors/generate_maze.py
@@ -1,42 +1,34 @@
 
 # Code for chapter 03 - Semantic Errors
 
-# WITH BUGS!
-# This code contains many defects.
-# Try to fix them all based on the program output.
-
-# HINT:
-# There are many defects in the code with overlapping defects.
-# Try to test the functions one by one.
-
 import random
 
-XMAX, YMAX = 12, 7
+XMAX, YMAX = 19, 16
 
 
-def create_grid_string(dots, xx, yy):
+def create_grid_string(dots, xsize, ysize):
     """
-    Creates a grid of size (xx, yy) 
+    Creates a grid of size (xx, yy)
     with the given positions of dots.
     """
-    for y in range(yy):
-        grid = ""
-        for x in range(xx):
-            grid = "#" if (xx, yy) in dots else "." # 3 defects in this line!
-        grid == "\n"
+    grid = ""
+    for y in range(ysize):
+        for x in range(xsize):
+            grid += "." if (x, y) in dots else "#"
+        grid += "\n"
     return grid
 
 
 def get_all_dot_positions(xsize, ysize):
     """Returns a list of (x, y) tuples covering all positions in a grid"""
-    return [(x,y) for x in range(0, xsize) for y in range(1, ysize-1)]
+    return [(x, y) for x in range(1, xsize - 1) for y in range(1, ysize - 1)]
 
 
 def get_neighbors(x, y):
     """Returns a list with the 8 neighbor positions of (x, y)"""
     return [
-        (x, -1), (y, x+1), (x-(1), y), (x+1), y,
-        (x,(-1, y)), (x+1, y, 1), (x-1, y+1, x+1, y+1)
+        (x, y-1), (x, y+1), (x-1, y), (x+1, y),
+        (x-1, y-1), (x+1, y-1), (x-1, y+1), (x+1, y+1)
         ]
 
 
@@ -48,9 +40,9 @@ def generate_dot_positions(xsize, ysize):
         x, y = random.choice(positions)
         neighbors = get_neighbors(x, y)
         free = [nb in dots for nb in neighbors]
-        if free.count(True) > 5:
+        if free.count(True) < 5:
             dots.add((x, y))
-    positions.remove((x, y))
+        positions.remove((x, y))
     return dots
 
 
@@ -58,10 +50,11 @@ def create_maze(xsize, ysize):
     """Returns a xsize*ysize maze as a string"""
     dots = generate_dot_positions(xsize, ysize)
     maze = create_grid_string(dots, xsize, ysize)
+    return maze
 
 
 if __name__ == '__main__':
-    dots = set(((1,1), (1,2), (1,3), (2,2), (3,1), (3,2), (3,3)))
+    dots = set(((1, 1), (1, 2), (1, 3), (2, 2), (3, 1), (3, 2), (3, 3)))
     print(create_grid_string(dots, 5, 5))
 
     positions = get_all_dot_positions(5, 5)
@@ -69,6 +62,6 @@ if __name__ == '__main__':
 
     neighbors = get_neighbors(3, 2)
     print(create_grid_string(neighbors, 5, 5))
-    
-    maze = create_maze
+
+    maze = create_maze(12, 7)
     print(maze)
